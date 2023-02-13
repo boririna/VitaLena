3416518bd62bc44b70648f386725b06513e56b14
Irina Borisova <i.a.borisova@gmail.com>
2023-02-13 13:45:10 +0100

    handler script change

--- js/handlers.js
+++ js/handlers.js
@@ -1,7 +1,24 @@
- const openNav = function () {
-    document.getElementById("mySidepanel").style.width = "50%";
-  }
+//  const openNav = function () {
+//     document.getElementById("mySidepanel").style.width = "50%";
+//   }
 
-  const closeNav = function () {
+//   const closeNav = function () {
+//     document.getElementById("mySidepanel").style.width = "0";
+//   }
+
+
+const btnEl = document.getElementById("openbtn");
+
+btnEl.addEventListener('click', openNav);
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "50%";
+}
+
+const btnEl2 = document.getElementById("closebtn");
+
+btnEl2.addEventListener('click', closeNav);
+
+function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
-  }
+}
