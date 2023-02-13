399300f6c48a6759b78482c9be2fbeb200a339fa
Irina Borisova <i.a.borisova@gmail.com>
2023-02-13 13:48:36 +0100

    onLoad in body

--- js/handlers.js
+++ js/handlers.js
@@ -1,24 +1,24 @@
-//  const openNav = function () {
-//     document.getElementById("mySidepanel").style.width = "50%";
-//   }
-
-//   const closeNav = function () {
-//     document.getElementById("mySidepanel").style.width = "0";
-//   }
-
-
-const btnEl = document.getElementById("openbtn");
-
-btnEl.addEventListener('click', openNav);
-
-function openNav() {
+ const openNav = function () {
     document.getElementById("mySidepanel").style.width = "50%";
-}
+  }
 
-const btnEl2 = document.getElementById("closebtn");
-
-btnEl2.addEventListener('click', closeNav);
-
-function closeNav() {
+  const closeNav = function () {
     document.getElementById("mySidepanel").style.width = "0";
-}
+  }
+
+
+// const btnEl = document.getElementById("openbtn");
+
+// btnEl.addEventListener('click', openNav);
+
+// function openNav() {
+//     document.getElementById("mySidepanel").style.width = "50%";
+// }
+
+// const btnEl2 = document.getElementById("closebtn");
+
+// btnEl2.addEventListener('click', closeNav);
+
+// function closeNav() {
+//     document.getElementById("mySidepanel").style.width = "0";
+// }
